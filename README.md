fa021d48c0d677c0e497ea2d7e139fc27336d681
Lucas M. Shepherd <lucas@thebluemark.com>
2021-04-16 00:59:37 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# wp-ganymede
+ LeoStella WordPress Theme
---
+++ functions.php
@@ -0,0 +1,8 @@
+<?php
+    add_action( 'wp_enqueue_scripts', 'enqueue_avada_child' );
+    function enqueue_avada_child()
+    {
+          wp_enqueue_style('avada-child-css', '/wp-content/themes/ganymede/style.css');
+          wp_enqueue_script('avada-child-js', '/wp-content/themes/ganymede/dist/js/script.js', array( 'jquery' ), '1.0', true );
+    }
+?>
